0e39d8486291db7e8796a2348cdb0c73ebfd4651
Josh Merritt <schruba@schruba.lan>
2020-10-12 11:30:56 -0700

    iOs fix, event listener

--- sketch.js
+++ sketch.js
@@ -75,6 +75,8 @@ let config = {
   function setup() {
     history.pushState({'page_id': 1}, document.title, location.href);
     playfield = createCanvas(windowWidth*config.xScale, windowHeight*config.yScale);
+    var el = document.getElementsByTagName("canvas")[0];
+    el.addEventListener("touchstart", mousePressed, false);
     setDisplaySize();
     engine = Matter.Engine.create();
     world = engine.world;
